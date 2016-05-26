b43b6dad86657a968496e57c55df6aed59cb5211
doctorgester <doctorgester@gmail.com>
2016-05-26 04:04:20 +0400

    Added some shitty 3d for no reason

--- src/main/java/com/dglab/cia/levelcreator/CIA.java
+++ src/main/java/com/dglab/cia/levelcreator/CIA.java
@@ -1,8 +1,8 @@
 package com.dglab.cia.levelcreator;
 
-import kn.uni.voronoitreemap.convexHull.JConvexHull;
-import kn.uni.voronoitreemap.convexHull.JFace;
-import kn.uni.voronoitreemap.convexHull.JVertex;
+import Jama.Matrix;
+import com.sun.javafx.geom.Vec3d;
+import com.sun.javafx.geom.Vec3f;
 import kn.uni.voronoitreemap.datastructure.OpenList;
 import kn.uni.voronoitreemap.diagram.PowerDiagram;
 import kn.uni.voronoitreemap.j2d.Point2D;
@@ -14,6 +14,7 @@ import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * User: kartemov
@@ -79,7 +80,7 @@ public class CIA {
         findInternalPoints();
         findPolygons();
 
-		new OBJSerializer().save("C:\\Users\\Toaru Shoujo\\Desktop\\m2.obj", polygons);
+        //new OBJSerializer().save("C:\\Users\\Toaru Shoujo\\Desktop\\m2.obj", polygons);
     }
 
     private void addPoint(Site site, Point2D point) {
@@ -177,7 +178,7 @@ public class CIA {
                 Point2D botRight = new Point2D(column + rowSize + 1, row + rowSize + 1);
                 Point2D botLeft = new Point2D(column - 1, row + rowSize + 1);
 
-                Rectangle2D rectangle2D = new Rectangle.Double(column - 1, row -1, rowSize + 2, rowSize + 2);
+                Rectangle2D rectangle2D = new Rectangle.Double(column - 1, row - 1, rowSize + 2, rowSize + 2);
                 PolygonSimple polygonSimple = new PolygonSimple();
                 polygonSimple.add(topLeft);
                 polygonSimple.add(topRight);
@@ -217,17 +218,95 @@ public class CIA {
     }
 
     private class View extends JPanel {
-        private void line(Graphics2D graphics2D, Point2D p1, Point2D p2) {
-            graphics2D.drawLine((int) p1.x, (int) p1.y, (int) p2.x, (int) p2.y);
+        private Vec3d cameraTo = new Vec3d(0, 0, 0);
+        private Vec3d cameraFrom = new Vec3d(2, 1, 0);
+
+        private Matrix camera = new Matrix(4, 4);
+        private Matrix projection = constructProjectionMatrix();
+
+        private Matrix constructProjectionMatrix() {
+            Matrix matrix = new Matrix(4, 4);
+
+            double h = 1 / Math.tan(Math.PI / 4);
+            double w = h;
+
+            double near = 0.1;
+            double far = 1000;
+
+            matrix.set(0, 0, w);
+            matrix.set(1, 1, h);
+            matrix.set(2, 2, far / (far - near));
+            matrix.set(3, 2, -near * far / (far - near));
+            matrix.set(2, 3, 1);
+            matrix.set(3, 3, 0);
+
+            return matrix;
         }
 
+        private void updateCameraMatrix() {
+            Vec3d n = new Vec3d(0, 0, 0);
+            n.sub(cameraTo, cameraFrom);
+            n.normalize();
+
+            Vec3d u = new Vec3d(0, 0, 0);
+            u.cross(new Vec3d(0, 1, 0), n);
+            u.normalize();
+
+            Vec3d v = new Vec3d(0, 0, 0);
+            v.cross(n, u);
+
+            camera = new Matrix(4, 4);
+
+            camera.set(0, 0, u.x);
+            camera.set(1, 0, u.y);
+            camera.set(2, 0, u.z);
+
+            camera.set(0, 1, v.x);
+            camera.set(1, 1, v.y);
+            camera.set(2, 1, v.z);
+
+            camera.set(0, 2, n.x);
+            camera.set(1, 2, n.y);
+            camera.set(2, 2, n.z);
+
+            camera.set(3, 0, -n.dot(cameraFrom));
+            camera.set(3, 1, -v.dot(cameraFrom));
+            camera.set(3, 2, -u.dot(cameraFrom));
+            camera.set(3, 3, 1);
+
+            camera = camera.transpose();
+
+            /*cameraFrom.x += 1;
+            cameraTo.x += 1;*/
+        }
+int x = 0;
         @Override
         public void paint(Graphics g) {
             super.paint(g);
 
+            Dimension size = getSize();
+
             Random random = new Random(1488);
 
             Graphics2D graphics2D = (Graphics2D) g;
+
+            updateCameraMatrix();
+
+            /*Matrix worldMatrix = Matrix.identity(4, 4);
+            x = x + 1;
+            worldMatrix.set(3, 0, x);*/
+
+            Matrix worldMatrix = Matrix.identity(4, 4);
+            //worldMatrix.set(0, 3, x / 5.0);
+            worldMatrix.set(1, 1, Math.cos(Math.toRadians(x)));
+            worldMatrix.set(2, 2, Math.cos(Math.toRadians(x)));
+            worldMatrix.set(1, 2, -Math.sin(Math.toRadians(x)));
+            worldMatrix.set(2, 1, Math.sin(Math.toRadians(x)));
+            x++;
+
+            Matrix viewProj = projection.times(camera).times(worldMatrix);
+
+            viewProj.transpose();
 
             graphics2D.setTransform(zoomAndPanListener.getCoordTransform());
 
@@ -239,10 +318,31 @@ public class CIA {
                 Color darker = color.darker();
 
                 for (PolygonSimple simple : simples) {
+                    List<Vec3d> transformed = new ArrayList<>();
+
+                    for (Point2D point2D : simple) {
+                        Matrix vertexMatrix = new Matrix(4, 1);
+                        vertexMatrix.set(0, 0, point2D.x);
+                        vertexMatrix.set(1, 0, 0);
+                        vertexMatrix.set(2, 0, point2D.y);
+                        vertexMatrix.set(3, 0, 1);
+
+                        Matrix computed = viewProj.times(vertexMatrix);
+
+                        double x = computed.get(0, 0);
+                        double y = computed.get(1, 0);
+                        double z = computed.get(2, 0);
+
+                        transformed.add(new Vec3d(x, y, z));
+                    }
+
+                    PolygonSimple transformedPolygon = new PolygonSimple();
+                    transformed.stream().map(v -> new Point2D(v.x, v.y)).forEach(transformedPolygon::add);
+
                     graphics2D.setColor(color);
-                    graphics2D.fill(simple);
+                    graphics2D.fill(transformedPolygon);
                     graphics2D.setColor(darker);
-                    graphics2D.draw(simple);
+                    graphics2D.draw(transformedPolygon);
                 }
             }
 
@@ -265,6 +365,8 @@ public class CIA {
             rawPoints.values().stream().flatMap(Collection::stream).forEach(point -> {
                 graphics2D.fillOval((int) point.x - 1, (int) point.y - 1, 2, 2);
             });*/
+
+            repaint(30);
         }
     }
 
@@ -303,18 +405,18 @@ public class CIA {
 
 
     public Point2D getLineIntersection2(
-            double x1,double y1,double x2,double y2,
-            double x3, double y3, double x4,double y4
+            double x1, double y1, double x2, double y2,
+            double x3, double y3, double x4, double y4
     ) {
-        double d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
+        double d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
         if (d == 0) return null;
 
-        double xi = ((x3-x4)*(x1*y2-y1*x2)-(x1-x2)*(x3*y4-y3*x4))/d;
-        double yi = ((y3-y4)*(x1*y2-y1*x2)-(y1-y2)*(x3*y4-y3*x4))/d;
+        double xi = ((x3 - x4) * (x1 * y2 - y1 * x2) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d;
+        double yi = ((y3 - y4) * (x1 * y2 - y1 * x2) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d;
 
-        Point2D p = new Point2D(xi,yi);
-        if (xi < Math.min(x1,x2) || xi > Math.max(x1,x2)) return null;
-        if (xi < Math.min(x3,x4) || xi > Math.max(x3,x4)) return null;
+        Point2D p = new Point2D(xi, yi);
+        if (xi < Math.min(x1, x2) || xi > Math.max(x1, x2)) return null;
+        if (xi < Math.min(x3, x4) || xi > Math.max(x3, x4)) return null;
         return p;
     }
 
